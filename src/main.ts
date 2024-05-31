fbc5c04587630d79c051f65918188b952e27464b
anindo-grype-kubuntu <anindo@grype.ca>
2024-05-31 10:16:12 +0600

    mdlwre ok

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { logger } from './middlewares/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,7 @@ async function bootstrap() {
     }),
   );
 
+  app.use(logger);
   await app.listen(5000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
