29ba752e9423d51fe11d3006dbdb882293a01f1f
anindo-grype-kubuntu <anindo@grype.ca>
2024-06-02 12:33:59 +0600

    middlware with validation pipe working

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { Cat } from 'src/cats/interfaces/cat.interface';
 import { CreateCatDto } from './dto/create-cat.dto';
@@ -14,11 +23,20 @@ export class CatsController {
 
   @Get()
   async findAll(): Promise<Cat[]> {
-    return this.catsService.findAll();
+    // return this.catsService.findAll();
+    throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string): string {
+  findOne(
+    @Param(
+      'id',
+      new ParseIntPipe({
+        errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE,
+      }),
+    )
+    id: number,
+  ): string {
     return `${id} cat`;
   }
 }
